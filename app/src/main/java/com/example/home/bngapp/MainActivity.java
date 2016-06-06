506601e31a71fadf461178f36520b750b55a51c5
rsorotzkin <rsorotzkin@gmail.com>
2016-06-06 14:06:16 -0400

    set up tabs

--- app/src/main/java/com/example/home/bngapp/MainActivity.java
+++ app/src/main/java/com/example/home/bngapp/MainActivity.java
@@ -1,32 +1,120 @@
 package com.example.home.bngapp;
 
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.ic_home_24dp,
+            R.drawable.ic_leaf_24dp,
+            R.drawable.ic_wallet_giftcard_24dp,
+            R.drawable.ic_favorite_outline_24dp,
+            R.drawable.ic_info_outline_24dp
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setSelectedTabIndicatorHeight(0);
+        setupTabIcons();
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager) {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "BNG2", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onTabSelected(TabLayout.Tab tab) {
+                super.onTabSelected(tab);
+                for (int i : tabIcons) {
+                    //if (tab.getPosition() == i)
+//                        Drawable mDrawable = getResources().getDrawable(i);
+//                    mDrawable.setColorFilter(new PorterDuffColorFilter(0xffff00, PorterDuff.Mode.MULTIPLY));
+//                    i.setColorFilter("#fff", PorterDuff.Mode.MULTIPLY);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                super.onTabUnselected(tab);
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                super.onTabReselected(tab);
             }
         });
+
+
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(), "Home");
+        adapter.addFragment(new MenuFragment(), "Menu");
+        adapter.addFragment(new GiftsFragment(), "Gifts");
+        adapter.addFragment(new BngLoveFragment(), "BngLove");
+        adapter.addFragment(new AboutFragment(), "About");
+        viewPager.setAdapter(adapter);
+
+
+    }
+
+    private void setupTabIcons() {
+
+        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabOne.setText("Home");
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_home_24dp, 0, 0);
+        tabLayout.getTabAt(0).setCustomView(tabOne);
+
+        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabTwo.setText("Menu");
+        tabTwo.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_leaf_24dp, 0, 0);
+        tabLayout.getTabAt(1).setCustomView(tabTwo);
+
+        TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabThree.setText("Gifts");
+        tabThree.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_wallet_giftcard_24dp, 0, 0);
+        tabLayout.getTabAt(2).setCustomView(tabThree);
+
+        TextView tabFour = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabFour.setText("BngLove");
+        tabFour.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_favorite_outline_24dp, 0, 0);
+        tabLayout.getTabAt(3).setCustomView(tabFour);
+
+        TextView tabFive = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabFive.setText("About");
+        tabFive.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_info_outline_24dp, 0, 0);
+        tabLayout.getTabAt(4).setCustomView(tabFive);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
